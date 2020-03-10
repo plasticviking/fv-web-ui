85132dd66dcb5b9412772ee1161d5cfe414c3738
Chad Oakenfold <web@oakenfold.ca>
2020-03-10 08:55:55 -0700

    FW-1013, FW-1042, FW-1063

--- frontend/app/assets/javascripts/views/pages/explore/dialect/learn/base/contributors-list-view.js
+++ frontend/app/assets/javascripts/views/pages/explore/dialect/learn/base/contributors-list-view.js
@@ -263,7 +263,7 @@ class ContributorsListView extends DataListView {
 const mapStateToProps = (state /*, ownProps*/) => {
   const { fvContributor, fvDialect, navigation, nuxeo, windowPath } = state
 
-  const { properties, route } = navigation
+  const { properties } = navigation
   const { computeLogin } = nuxeo
   const { computeContributors } = fvContributor
   const { computeDialect2 } = fvDialect
@@ -273,7 +273,6 @@ const mapStateToProps = (state /*, ownProps*/) => {
     computeContributors,
     computeDialect2,
     computeLogin,
-    navigationRouteSearch: route.search,
     properties,
     splitWindowPath,
     windowPath: _windowPath,
