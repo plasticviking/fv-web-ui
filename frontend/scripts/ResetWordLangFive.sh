dc18a51364e755b5dfcdc4d9c80096a3f462051a
Nolan Van Hell <nolan@fpcc.ca>
2020-03-17 16:21:57 -0700

    Fix docker backend url issue, FW-1049 Cypress fixes, update README (#224)
    * Fix docker backend url issue and update README
    
    * Update ResetWordLangFive.sh script
    
    * Revert diffset action step update due to bug in new version
    
    * Fix PhrasebookDelete.js Cypress test

--- frontend/scripts/ResetWordLangFive.sh
+++ frontend/scripts/ResetWordLangFive.sh
@@ -7,19 +7,22 @@
 DIRECTORY=$PWD
 echo $DIRECTORY 1>/dev/null
 
-# Delete existing Dictionary directory and all files
+# Delete existing TestLanguageFive directory and all files
 cd $DIRECTORY/temp/fv-utils-temp/target
-java -jar fv-nuxeo-utils-*.jar clear-words -username $CYPRESS_FV_USERNAME -password $CYPRESS_FV_PASSWORD -url "$TARGET/nuxeo" -language-directory Test/Test/ -language-name TestLanguageFive 1>/dev/null
+java -jar fv-nuxeo-utils-*.jar delete-language -username $CYPRESS_FV_USERNAME -password $CYPRESS_FV_PASSWORD -url $TARGET/nuxeo -language-directory Test/Test/ -language-name TestLanguageFive 1>/dev/null
 if [[ "$?" -ne 0 ]]; then
-  echo -e 'fv-utils TestLanguageFive dictionary clear failed \n'; exit 1
+  echo -e 'fv-utils TestLanguageFive teardown failed \n'; exit 1
 fi
-# Publish the fresh dictionary to match the language
-echo "Publishing dictionary" 1>/dev/null
-response=$(curl -o /dev/null -s -w "%{response_code}\n" -X POST ${TARGET}'/nuxeo/site/automation/FVPublish' -H 'Nuxeo-Transaction-Timeout: 10' -H 'X-NXproperties: *' -H 'X-NXRepository: default' -H 'X-NXVoidOperation: false' -H 'content-type: application/json' -d '{"params":{},"input":"/FV/Workspaces/Data/Test/Test/TestLanguageFive/Dictionary","context":{}}' -u $CYPRESS_FV_USERNAME:$CYPRESS_FV_PASSWORD) 1>/dev/null
+# Create a fresh TestLanguageFive directory and all files
+java -jar fv-nuxeo-utils-*.jar create-language -username $CYPRESS_FV_USERNAME -password $CYPRESS_FV_PASSWORD -url $TARGET/nuxeo -language-directory Test/Test/ -language-name TestLanguageFive 1>/dev/null
+if [[ "$?" -ne 0 ]]; then
+  echo -e 'fv-utils TestLanguageFive creation failed \n'; exit 1
+fi
+# Publish the language TestLanguageFive
+response=$(curl -o /dev/null -s -w "%{response_code}\n" -X POST ${TARGET}'/nuxeo/site/automation/javascript.FVPublishOrRepublish' -H 'Nuxeo-Transaction-Timeout: 10' -H 'X-NXproperties: *' -H 'X-NXRepository: default' -H 'X-NXVoidOperation: false' -H 'content-type: application/json' -d '{"params":{},"input":"/FV/Workspaces/Data/Test/Test/TestLanguageFive","context":{}}' -u $CYPRESS_FV_USERNAME:$CYPRESS_FV_PASSWORD) 1>/dev/null
 if [[ "$response" -ne 200 ]]; then
-    echo -e 'TestLanguageFive dictionary publish failed \n'; exit $response
+    echo -e 'TestLanguageFive publish failed: Error ' $response ' \n'; exit 1
 fi
-
 # Import Word using fv-batch-import
 cd $DIRECTORY/temp/fv-batch-import-temp/target
 java -jar fv-batch-import-*.jar -url "$TARGET/nuxeo" -username $CYPRESS_FV_USERNAME -password $CYPRESS_FV_PASSWORD -domain FV -csv-file $DIRECTORY/scripts/files/testLangFiveWord.csv -data-path $DIRECTORY/scripts/files/testLangTwoMedia/ -dialect-id fillerID -language-path Test/Test/TestLanguageFive 1>/dev/null
@@ -39,13 +42,12 @@ if [[ $count != 0 ]]; then
     echo "Removing generated batch files" 1>/dev/null
     rm *_errors.csv
 fi
-
 # If the parameter "enabled-true" is found then send a CURL request to enable the word
 if [[ $1 == 'enabled-true' ]]; then
     echo "Enabling word" 1>/dev/null
     responseOne=$(curl -o /dev/null -s -w "%{response_code}\n" -X POST ${TARGET}'/nuxeo/site/automation/FVEnableDocument' -H 'Nuxeo-Transaction-Timeout: 10' -H 'X-NXproperties: *' -H 'X-NXRepository: default' -H 'X-NXVoidOperation: false' -H 'content-type: application/json' -d '{"params":{},"input":"/FV/Workspaces/Data/Test/Test/TestLanguageFive/Dictionary/TestWord","context":{}}' -u $CYPRESS_FV_USERNAME:$CYPRESS_FV_PASSWORD) 1>/dev/null
     if [[ "$responseOne" -ne 200 ]]; then
-        echo -e 'TestLanguageFive word enable failed \n'; exit $responseOne
+        echo -e 'TestLanguageFive word enable failed: Error ' $responseOne ' \n'; exit $responseOne
     fi
 fi
 # If the parameter "enabled-true" is found then send a CURL request to enable the phrase
@@ -53,7 +55,7 @@ if [[ $1 == 'enabled-true' ]]; then
     echo "Enabling phrase" 1>/dev/null
     responseTwo=$(curl -o /dev/null -s -w "%{response_code}\n" -X POST ${TARGET}'/nuxeo/site/automation/FVEnableDocument' -H 'Nuxeo-Transaction-Timeout: 10' -H 'X-NXproperties: *' -H 'X-NXRepository: default' -H 'X-NXVoidOperation: false' -H 'content-type: application/json' -d '{"params":{},"input":"/FV/Workspaces/Data/Test/Test/TestLanguageFive/Dictionary/TestPhrase","context":{}}' -u $CYPRESS_FV_USERNAME:$CYPRESS_FV_PASSWORD) 1>/dev/null
     if [[ "$responseTwo" -ne 200 ]]; then
-        echo -e 'TestLanguageFive phrase enable failed \n'; exit $responseTwo
+        echo -e 'TestLanguageFive phrase enable failed: Error ' $responseTwo ' \n'; exit $responseTwo
     fi
 fi
 
