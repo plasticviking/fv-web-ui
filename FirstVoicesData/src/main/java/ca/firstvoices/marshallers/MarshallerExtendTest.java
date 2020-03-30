720b76700cd87ce9f07f4b5fe02b0d9f9256f288
Nolan Van Hell <nolan@fpcc.ca>
2020-03-30 10:36:52 -0700

    FW-1151 Marshaller testing (WIP)

--- FirstVoicesData/src/main/java/ca/firstvoices/marshallers/MarshallerExtendTest.java
+++ FirstVoicesData/src/main/java/ca/firstvoices/marshallers/MarshallerExtendTest.java
@@ -9,7 +9,8 @@ import org.nuxeo.ecm.core.io.registry.reflect.Priorities;
 import org.nuxeo.ecm.core.io.registry.reflect.Setup;
 
 import java.io.IOException;
-import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 /*
     This marshaller will add the field "additional" with the value "information" to any endpoints which return a DocumentModel
@@ -19,15 +20,20 @@ public class MarshallerExtendTest extends DocumentModelJsonWriter {
     public void extend(DocumentModel document, JsonGenerator jg) throws IOException {
         super.extend(document, jg);
         CoreSession session = document.getCoreSession();
-        int major_ver = Integer.parseInt(document.getPropertyValue("uid:major_version").toString());
-        String parentLanguageID = document.getPropertyValue("fva:language").toString();
-        DocumentRef ref = new IdRef(parentLanguageID);
-        DocumentModel doc = session.getDocument(ref);
-        Object testObj[] = new Object[5];
-        testObj[0] = doc.getPropertyValue("dc:title");
-        testObj[1] = doc.getPropertyValue("fva:family");
-//        jg.writeObjectField("parentLanguage", doc.getPropertyValue("dc:title"));
-//        jg.writeObjectField("parentLanguageFamily", doc.getPropertyValue("fva:family"));
-        jg.writeObjectField("parentLanguageFamily", testObj);
+        String parentLanguageID = (String) document.getPropertyValue("fva:language");
+        if (parentLanguageID != null) {
+            DocumentRef ref = new IdRef(parentLanguageID);
+            DocumentModel doc = session.getDocument(ref);
+            Map<String, Object> map = new HashMap<>();
+            Map<String, Object> properties = new HashMap<>();
+            map.put("dc:title", doc.getPropertyValue("dc:title"));
+            map.put("fva:family", doc.getPropertyValue("fva:family"));
+            map.put("state", doc.getCurrentLifeCycleState());
+            map.put("path", doc.getPath().toString());
+            properties.put("uid:major_version", doc.getPropertyValue("uid:major_version"));
+            properties.put("uid:minor_version", doc.getPropertyValue("uid:minor_version"));
+            map.put("properties", properties);
+            jg.writeObjectField("parentLanguageFamily", map);
+        }
     }
 }
