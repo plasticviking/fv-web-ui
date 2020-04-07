2b5bd4dc7db98b26e89e228ab748d903abcdf424
David Lacho <david.lacho@gmail.com>
2020-04-07 14:39:19 -0700

    FW-1210: Create a scheduler for re-alphabetizing archives (#248)
    * FW-1140: Scheduling Example
    
    * FW-1140: Scheduling Fixes
    
    * FW-1140: Scheduling Fixes
    
    * FW-1210: Create a scheduler for alphabetizing archives
    
    * FW-1210: Refactors to have schedule run every 10 minutes
    
    * FW-1210: Readability Refactors

--- FirstVoicesPublisher/src/main/java/ca/firstvoices/nativeorder/services/NativeOrderComputeServiceImpl.java
+++ FirstVoicesPublisher/src/main/java/ca/firstvoices/nativeorder/services/NativeOrderComputeServiceImpl.java
@@ -1,22 +1,26 @@
 /*
- * Copyright 2016 First People's Cultural Council
+ *
+ * Copyright 2020 First People's Cultural Council
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * /
  */
 package ca.firstvoices.nativeorder.services;
 
+import ca.firstvoices.services.AbstractService;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentModelList;
-
-import ca.firstvoices.services.AbstractService;
 import org.nuxeo.ecm.core.api.PathRef;
 
 import java.util.Arrays;
@@ -29,7 +33,7 @@ import java.util.stream.Collectors;
  */
 public class NativeOrderComputeServiceImpl extends AbstractService implements NativeOrderComputeService {
 
-    private DocumentModel[] loadAlphabet(DocumentModel dialect) {
+    private DocumentModel[] loadCharacters(DocumentModel dialect) {
         DocumentModelList chars = dialect.getCoreSession().getChildren(new PathRef(dialect.getPathAsString() + "/Alphabet"));
         return chars
                 .stream()
@@ -52,7 +56,7 @@ public class NativeOrderComputeServiceImpl extends AbstractService implements Na
             DocumentModel dialect = getDialect(asset);
             CoreSession session = asset.getCoreSession();
             // First get the native alphabet
-            DocumentModel[] chars = loadAlphabet(dialect);
+            DocumentModel[] chars = loadCharacters(dialect);
             computeNativeOrderTranslation(chars, asset);
         }
     }
@@ -65,11 +69,14 @@ public class NativeOrderComputeServiceImpl extends AbstractService implements Na
     public void computeDialectNativeOrderTranslation(DocumentModel dialect) {
         CoreSession session = dialect.getCoreSession();
         // First get the native alphabet
-        DocumentModel[] chars = loadAlphabet(dialect);
+        DocumentModel[] chars = loadCharacters(dialect);
         computeNativeOrderTranslation(chars,
                 session.query("SELECT * FROM FVWord WHERE ecm:ancestorId='" + dialect.getId() + "'"));
         computeNativeOrderTranslation(chars,
                 session.query("SELECT * FROM FVPhrase WHERE ecm:ancestorId='" + dialect.getId() + "'"));
+        DocumentModel alphabet = session.getDocument(new PathRef(dialect.getPathAsString() + "/Alphabet"));
+        alphabet.setPropertyValue("custom_order_recompute_required", false);
+        session.saveDocument(alphabet);
         session.save();
     }
 
@@ -80,7 +87,7 @@ public class NativeOrderComputeServiceImpl extends AbstractService implements Na
         }
 
         String title = (String) element.getPropertyValue("dc:title");
-        String nativeTitle = "";
+        StringBuilder nativeTitle = new StringBuilder();
         List<String> fvChars =
                 Arrays.stream(chars).map(character -> (String) character.getPropertyValue("dc:title")).collect(Collectors.toList());
         List<String> upperChars = Arrays.stream(chars).map(character -> (String) character.getPropertyValue(
@@ -99,7 +106,7 @@ public class NativeOrderComputeServiceImpl extends AbstractService implements Na
                 String ucCharValue = (String) charDoc.getPropertyValue("fvcharacter:upper_case_character");
 
                 if (isCorrectCharacter(title, fvChars, upperChars, charValue, ucCharValue)) {
-                    nativeTitle += new Character((char) (34 + (Long) charDoc.getPropertyValue("fvcharacter:alphabet_order"))).toString();
+                    nativeTitle.append((char) (34 + (Long) charDoc.getPropertyValue("fvcharacter:alphabet_order")));
                     title = title.substring(charValue.length());
                     found = true;
                     break;
@@ -107,9 +114,9 @@ public class NativeOrderComputeServiceImpl extends AbstractService implements Na
             }
             if (!found) {
                 if (" ".equals(title.substring(0, 1))) {
-                    nativeTitle += "!";
+                    nativeTitle.append("!");
                 } else {
-                    nativeTitle += "~" + title.substring(0, 1);
+                    nativeTitle.append("~").append(title, 0, 1);
                 }
                 title = title.substring(1);
             }
@@ -119,9 +126,9 @@ public class NativeOrderComputeServiceImpl extends AbstractService implements Na
         // In the case that the sorting methods are the same,
         // we don't want to trigger subsequent events that are listening for a save.
         // Just keep the sorting order on the document as it was. No need to save.
-        if (originalCustomSort == null || !nativeTitle.equals(originalCustomSort)) {
+        if (!nativeTitle.toString().equals(originalCustomSort)) {
             if (!element.isImmutable()) {
-                element.setPropertyValue("fv:custom_order", nativeTitle);
+                element.setPropertyValue("fv:custom_order", nativeTitle.toString());
             }
             element.getCoreSession().saveDocument(element);
         }
@@ -137,18 +144,16 @@ public class NativeOrderComputeServiceImpl extends AbstractService implements Na
                                        String ucCharValue) {
 
         if ((title.startsWith(charValue)) || (ucCharValue != null && title.startsWith(ucCharValue))) {
-            boolean incorrect = false;
+            boolean incorrect;
 
-            if (charValue != null) {
-                // Grab all the characters that begin with the current character (for example, if "current character" is
-                // iterating on "a", it will return "aa" if it is also in the alphabet)
-                List<String> charsStartingWithCurrentCharLower =
-                        fvChars.stream().filter(character -> character != null ? character.startsWith(charValue) : false).collect(Collectors.toList());
-                // Go through the characters that begin with the "current character", and ensure that the title does not
-                // start with any character in that list (save for the "current character" that we're iterating on).
-                incorrect =
-                        charsStartingWithCurrentCharLower.stream().anyMatch(character -> !character.equals(charValue) && title.startsWith(character));
-            }
+            // Grab all the characters that begin with the current character (for example, if "current character" is
+            // iterating on "a", it will return "aa" if it is also in the alphabet)
+            List<String> charsStartingWithCurrentCharLower =
+                    fvChars.stream().filter(character -> character != null && character.startsWith(charValue)).collect(Collectors.toList());
+            // Go through the characters that begin with the "current character", and ensure that the title does not
+            // start with any character in that list (save for the "current character" that we're iterating on).
+            incorrect =
+                    charsStartingWithCurrentCharLower.stream().anyMatch(character -> !character.equals(charValue) && title.startsWith(character));
             // If there is no match and the character has an uppercase equivalent, we want to repeat the process
             // above with uppercase character. We also check the lowercase in an example of yZ is the "uppercase" of yz.
             if (ucCharValue != null && !incorrect) {
@@ -156,12 +161,8 @@ public class NativeOrderComputeServiceImpl extends AbstractService implements Na
                         upperChars.stream().filter(character -> {
                             if (character == null) {
                                 return false;
-                            } else if (charValue != null) {
-                                return character.startsWith(ucCharValue) || character.startsWith(charValue);
-                            } else if (charValue == null) {
-                                return character.startsWith(ucCharValue);
                             }
-                            return false;
+                            return character.startsWith(ucCharValue) || character.startsWith(charValue);
                         }).collect(Collectors.toList());
                 incorrect =
                         charsStartingWithCurrentCharUpper.stream().anyMatch(uCharacter -> !uCharacter.equals(ucCharValue) && title.startsWith(uCharacter));
