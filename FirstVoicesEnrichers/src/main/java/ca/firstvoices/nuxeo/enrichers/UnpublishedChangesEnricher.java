fe4e3b412e4e6098717be8ce4a3067e3a80b5cad
Daniel Yona <dy2288@users.noreply.github.com>
2020-05-15 15:09:50 -0700

    FW-1431 - ensure unpublished enricher is unpriveleged (#322)

--- FirstVoicesEnrichers/src/main/java/ca/firstvoices/nuxeo/enrichers/UnpublishedChangesEnricher.java
+++ FirstVoicesEnrichers/src/main/java/ca/firstvoices/nuxeo/enrichers/UnpublishedChangesEnricher.java
@@ -10,92 +10,89 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.io.IOException;
+import java.util.Arrays;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.CoreInstance;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.io.marshallers.json.enrichers.AbstractJsonEnricher;
 import org.nuxeo.ecm.core.io.registry.reflect.Setup;
 import org.nuxeo.ecm.core.schema.DocumentType;
 import org.nuxeo.runtime.api.Framework;
 
-import java.io.IOException;
-import java.util.Arrays;
-
 @Setup(mode = SINGLETON, priority = REFERENCE)
 public class UnpublishedChangesEnricher extends AbstractJsonEnricher<DocumentModel> {
 
-    private static final Log log = LogFactory.getLog(UnpublishedChangesEnricher.class);
+  public static final String NAME = "unpublished_changes";
+  private static final Log log = LogFactory.getLog(UnpublishedChangesEnricher.class);
+  protected UnpublishedChangesService service = Framework
+      .getService(UnpublishedChangesService.class);
 
-    public static final String NAME = "unpublished_changes";
+  public UnpublishedChangesEnricher() {
+    super(NAME);
+  }
 
-    public UnpublishedChangesEnricher() {
-        super(NAME);
-    }
+  // Method that will be called when the enricher is asked for
+  @Override
+  public void write(JsonGenerator jg, DocumentModel doc) throws IOException {
+    // We use the Jackson library to generate Json
+    ObjectNode unpublishedChangesJsonObject = constructUnpublishedChangesJSON(doc);
+    jg.writeFieldName(NAME);
+    jg.writeObject(unpublishedChangesJsonObject);
+  }
 
-    protected UnpublishedChangesService service = Framework.getService(UnpublishedChangesService.class);
+  private ObjectNode constructUnpublishedChangesJSON(DocumentModel doc)
+      throws JsonGenerationException, JsonMappingException, IOException {
+    ObjectMapper mapper = new ObjectMapper();
 
-    // Method that will be called when the enricher is asked for
-    @Override
-    public void write(JsonGenerator jg, DocumentModel doc) throws IOException {
-        // We use the Jackson library to generate Json
-        ObjectNode unpublishedChangesJsonObject = constructUnpublishedChangesJSON(doc);
-        jg.writeFieldName(NAME);
-        jg.writeObject(unpublishedChangesJsonObject);
-    }
+    return CoreInstance.doPrivileged(doc.getCoreSession(), session -> {
+      // JSON object to be returned
+      ObjectNode jsonObj = mapper.createObjectNode();
 
-    private ObjectNode constructUnpublishedChangesJSON(DocumentModel doc)
-            throws JsonGenerationException, JsonMappingException, IOException {
-        ObjectMapper mapper = new ObjectMapper();
+      log.debug("Constructing unpublished changes for doc: " + doc.getId());
 
-        // JSON object to be returned
-        ObjectNode jsonObj = mapper.createObjectNode();
+      // If the document is the correct type then check for unpublished changes using the service.
+      if (checkType(doc)) {
+        boolean unpublishedChanges = service.checkUnpublishedChanges(session, doc);
 
-        log.debug("Constructing unpublished changes for doc: " + doc.getId());
+        jsonObj.put("unpublished_changes_exist", unpublishedChanges);
+      }
 
-        // First create the parent document's Json object content
-        CoreSession session = doc.getCoreSession();
+      return jsonObj;
+    });
+  }
 
-        // If the document is the correct type then check for unpublished changes using the service.
-        if (checkType(doc)) {
-            boolean unpublishedChanges = service.checkUnpublishedChanges(session, doc);
+  // Helper method to check that the new document is one of the types below
+  private boolean checkType(DocumentModel inputDoc) {
+    DocumentType currentType = inputDoc.getDocumentType();
 
-            jsonObj.put("unpublished_changes_exist", unpublishedChanges);
-        }
-
-        return jsonObj;
-    }
-
-    // Helper method to check that the new document is one of the types below
-    private boolean checkType(DocumentModel inputDoc) {
-        DocumentType currentType = inputDoc.getDocumentType();
-
-        String[] types = {
-                "FVAlphabet",
-                "FVAudio",
-                "FVBook",
-                "FVBookEntry",
-                "FVBooks",
-                "FVCategories",
-                "FVCategory",
-                "FVCharacter",
-                "FVContributor",
-                "FVContributors",
-                "FVDialect",
-                "FVDictionary",
-                "FVGallery",
-                "FVLanguage",
-                "FVLanguageFamily",
-                "FVLink",
-                "FVLinks",
-                "FVPhrase",
-                "FVPicture",
-                "FVPortal",
-                "FVResources",
-                "FVVideo",
-                "FVWord",
-        };
-        return Arrays.stream(types).parallel().anyMatch(currentType.toString()::contains);
-    }
+    String[] types = {
+        "FVAlphabet",
+        "FVAudio",
+        "FVBook",
+        "FVBookEntry",
+        "FVBooks",
+        "FVCategories",
+        "FVCategory",
+        "FVCharacter",
+        "FVContributor",
+        "FVContributors",
+        "FVDialect",
+        "FVDictionary",
+        "FVGallery",
+        "FVLanguage",
+        "FVLanguageFamily",
+        "FVLink",
+        "FVLinks",
+        "FVPhrase",
+        "FVPicture",
+        "FVPortal",
+        "FVResources",
+        "FVVideo",
+        "FVWord",
+    };
+    return Arrays.stream(types).parallel().anyMatch(currentType.toString()::contains);
+  }
 
 }
