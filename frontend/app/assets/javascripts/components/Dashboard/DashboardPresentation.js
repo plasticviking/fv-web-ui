d483982ab6dc68ca648b1e5c6df80fd6681437bd
Bronson Chapman <64934597+bronson-c@users.noreply.github.com>
2020-07-23 11:42:14 -0600

    FW 1613 - Two column dashboard (#423)
    * FW-1613: Add two column dashboard view that changes to stacked on a small screen
    
    * FW-1613: Remove div tag border, add comment

--- frontend/app/assets/javascripts/components/Dashboard/DashboardPresentation.js
+++ frontend/app/assets/javascripts/components/Dashboard/DashboardPresentation.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Typography from '@material-ui/core/Typography'
 
+import '!style-loader!css-loader!./Dashboard.css'
+
 /**
  * @summary DashboardPresentation
  * @version 1.0.1
@@ -18,10 +20,15 @@ function DashboardPresentation({ children }) {
       <Typography variant="h4" component="h1">
         Dashboard
       </Typography>
-      {children}
+      <div className="Dashboard">
+        <div>{children}</div>
+        <div>{children}</div>
+        {/*Remove line before release, for testing multiple columns*/}
+      </div>
     </>
   )
 }
+
 // PROPTYPES
 const { node } = PropTypes
 DashboardPresentation.propTypes = {
