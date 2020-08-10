8cfbf28b93910049317934ba9b3ac4ff77977d92
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2020-08-10 14:41:32 -0700

    FW-1741 (#437)
    * Set up Data layer for VisibilitySelect to check if dialect is public
    
    * Rendering Visibility options based on dialect state
    
    * Use ProviderHelper fetchIfMissing

--- frontend/app/assets/javascripts/common/DataSource/useDialect.js
+++ frontend/app/assets/javascripts/common/DataSource/useDialect.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchDialect2 as _fetchDialect2 } from 'providers/redux/reducers/fvDialect'
+import { fetchDialect as _fetchDialect, fetchDialect2 as _fetchDialect2 } from 'providers/redux/reducers/fvDialect'
 
 function useDialect() {
   const dispatch = useDispatch()
@@ -8,9 +8,16 @@ function useDialect() {
     const dispatchObj = _fetchDialect2(pathOrId, messageStart, messageSuccess, messageError, propertiesOverride)
     dispatch(dispatchObj)
   }
+
+  const fetchDialect = (pathOrId, messageStart, messageSuccess, messageError, propertiesOverride) => {
+    const dispatchObj = _fetchDialect(pathOrId, messageStart, messageSuccess, messageError, propertiesOverride)
+    dispatch(dispatchObj)
+  }
+
   return {
     computeDialect: useSelector((state) => state.fvDialect.computeDialect),
     computeDialect2: useSelector((state) => state.fvDialect.computeDialect2),
+    fetchDialect,
     fetchDialect2,
   }
 }
--- frontend/app/assets/javascripts/components/VisibilitySelect/VisibilitySelectContainer.js
+++ frontend/app/assets/javascripts/components/VisibilitySelect/VisibilitySelectContainer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import VisibilitySelectData from './VisibilitySelectData'
 import VisibilitySelectPresentation from './VisibilitySelectPresentation'
 import PromiseWrapper from 'views/components/Document/PromiseWrapper'
 
@@ -12,17 +13,27 @@ import PromiseWrapper from 'views/components/Document/PromiseWrapper'
  * @param {string} docVisibility A string with the value of 'teams', 'members', or, 'public that reflects the visibility of the document being viewed.
  * @param {function} handleVisibilityChange A function to handle the onChange of the select component
  * @param {object} computeEntities An Immutable.fromJS object required by the PromiseWrapper Component
+ * @param {boolean} hideLabel If true the label: "Who can see this?", will be hidden
  *
  * @returns {node} jsx markup
  */
 function VisibilitySelectContainer({ docVisibility, handleVisibilityChange, computeEntities, hideLabel }) {
   return (
     <PromiseWrapper renderOnError computeEntities={computeEntities}>
-      <VisibilitySelectPresentation
-        docVisibility={docVisibility}
-        handleVisibilityChange={handleVisibilityChange}
-        hideLabel={hideLabel}
-      />
+      <VisibilitySelectData>
+        {({ publicDialect, isLoading }) => {
+          return isLoading ? (
+            <div>Loading...</div>
+          ) : (
+            <VisibilitySelectPresentation
+              docVisibility={docVisibility}
+              handleVisibilityChange={handleVisibilityChange}
+              hideLabel={hideLabel}
+              publicDialect={publicDialect}
+            />
+          )
+        }}
+      </VisibilitySelectData>
     </PromiseWrapper>
   )
 }
