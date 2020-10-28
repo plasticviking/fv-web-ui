9afe5ccdc0d3f69210006d114b0c3974c22b3b60
Chad Oakenfold <web@oakenfold.ca>
2020-10-28 11:15:20 -0700

    FW-1955 (#554)
    Co-authored-by: Daniel Yona <dy2288@users.noreply.github.com>
    Co-authored-by: Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>

--- frontend/app/components/PageToolbar/PageToolbar.js
+++ frontend/app/components/PageToolbar/PageToolbar.js
@@ -108,6 +108,13 @@ export class PageToolbar extends Component {
         This site is <strong>private</strong>. Contact hello@firstvoices.com to make it public.
       </div>
     )
+    const getDocState = (_computeEntity) => {
+      const state = selectn('response.state', _computeEntity)
+      if (state === 'Republished') {
+        return 'Published'
+      }
+      return state
+    }
 
     return (
       <AppBar color="primary" position="static" classes={classes} className="PageToolbar">
@@ -127,7 +134,7 @@ export class PageToolbar extends Component {
               <AuthorizationFilter filter={{ permission: 'Write', entity: selectn('response', computeEntity) }}>
                 <VisibilityMinimal.Container
                   docId={selectn('response.uid', computeEntity)}
-                  docState={selectn('response.state', computeEntity)}
+                  docState={getDocState(computeEntity)}
                   computeEntities={computeEntities || Immutable.List()}
                 />
               </AuthorizationFilter>
