a2509fe0d24fec3af4246798630fac29298fb691
Robert Johnstone <rob@plasticviking.com>
2021-02-04 03:55:41 +0800

    Add typeadapters to accept json inputs for bulk update operation (#673)
    * Add typeadapters to accept json inputs for bulk update operation
    
    * sonar appeasement
    
    Co-authored-by: Daniel Yona <dy2288@users.noreply.github.com>

--- modules/common/firstvoices-operations/src/main/java/ca/firstvoices/operations/bulkupdate/services/BulkUpdateService.java
+++ modules/common/firstvoices-operations/src/main/java/ca/firstvoices/operations/bulkupdate/services/BulkUpdateService.java
@@ -2,12 +2,13 @@ package ca.firstvoices.operations.bulkupdate.services;
 
 import ca.firstvoices.operations.bulkupdate.BulkUpdateMode;
 import java.io.Serializable;
+import java.util.List;
 import org.nuxeo.ecm.core.api.CoreSession;
-import org.nuxeo.ecm.core.api.DocumentRefList;
+import org.nuxeo.ecm.core.api.DocumentRef;
 
 public interface BulkUpdateService {
 
   void bulkUpdate(
-      CoreSession session, DocumentRefList refs, BulkUpdateMode mode, String field,
+      CoreSession session, List<DocumentRef> refs, BulkUpdateMode mode, String field,
       Serializable value);
 }
