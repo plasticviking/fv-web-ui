41067b43c5707f3f554bf131abc0ccd47f42f714
Robert Johnstone <rob@plasticviking.com>
2021-03-16 19:31:11 +0800

    remove extraneous import

--- modules/api/firstvoices-rest-page-providers/src/main/java/ca/firstvoices/rest/data/SearchResult.java
+++ modules/api/firstvoices-rest-page-providers/src/main/java/ca/firstvoices/rest/data/SearchResult.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.Nullable;
-import org.hibernate.annotations.Parent;
+
 
 public class SearchResult implements Serializable {
 
