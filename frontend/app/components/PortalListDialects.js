f9be152092eea04c34cda8295c74fcb42afd4977
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2021-06-16 16:26:07 -0700

    Update property label for joinText

--- frontend/app/components/PortalListDialects.js
+++ frontend/app/components/PortalListDialects.js
@@ -77,7 +77,7 @@ export class PortalListDialects extends Component {
     const href = encodeURI(`/${this.props.siteTheme}${tile.path.replace('/Portal', '')}`)
     const dialectTitle = selectn('title', tile)
     const dialectGroups = selectn('groups', tile) || []
-    const joinText = selectn('fvdialect:join_text', tile) || 'Are you a member of the community or language team?'
+    const joinText = selectn('joinText', tile) || 'Are you a member of the community or language team?'
 
     return (
       <DialectTile
